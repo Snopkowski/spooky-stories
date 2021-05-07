e0102894600e6cecb98424f3331f5e98cc6b9901
Snopkowski <wojciechsnopkowski@gmail.com>
2021-05-07 21:25:40 +0100

    hotfix--input on username form

--- pages/enter.js
+++ pages/enter.js
@@ -2,24 +2,25 @@ import { auth, firestore, googleAuthProvider } from '../lib/firebase';
 import { UserContext } from '../lib/context';
 import {
   Button,
-  Box,
   Flex,
   Avatar,
   SimpleGrid,
   Heading,
   Center,
+  Text,
+  Stack,
+  Input,
 } from '@chakra-ui/react';
 
 import { useEffect, useState, useCallback, useContext } from 'react';
 import debounce from 'lodash.debounce';
-import { Router, useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 
 export default function Enter(props) {
   const { user, username } = useContext(UserContext);
 
-  // 1. user signed out <SignInButton />
-  // 2. user signed in, but missing username <UsernameForm />
-  // 3. user signed in, has username <SignOutButton />
+  const router = useRouter();
+
   return (
     <Flex as='main'>
       {user ? (
@@ -37,19 +38,18 @@ export default function Enter(props) {
 
 // Sign in with Google button
 function SignInButton() {
-  const router = useRouter();
   const signInWithGoogle = async () => {
-    await (auth.signInWithPopup(googleAuthProvider) && router.push('/'));
+    await auth.signInWithPopup(googleAuthProvider);
   };
 
   return (
     <SimpleGrid w='100%' columns={2} minChildWidth={300}>
-      <Center>
+      <Center py='8'>
         <Button onClick={signInWithGoogle}>
           <Avatar mr={4} size='xs' src={'./vercel.png'} /> Sign in with Google
         </Button>
       </Center>
-      <Center>Make your own account to share your story.</Center>
+      <Center py='8'>Make your own account to share your story.</Center>
     </SimpleGrid>
   );
 }
@@ -78,7 +78,7 @@ function UsernameForm() {
 
     const batch = firestore.batch();
     batch.set(userDoc, {
-      username: formValue,
+      username: [formValue],
       photoURL: user.photoURL,
       displayName: user.displayName,
     });
@@ -113,7 +113,6 @@ function UsernameForm() {
       if (username.length >= 3) {
         const ref = firestore.doc(`usernames/${username}`);
         const { exists } = await ref.get();
-        console.log('Firestore read executed!');
         setIsValid(!exists);
         setLoading(false);
       }
@@ -123,46 +122,50 @@ function UsernameForm() {
 
   return (
     !username && (
-      <section>
-        <Heading as='h3'>Choose Username</Heading>
+      <Stack spacing={8} as='section'>
+        <Heading as='h3'>Choose your Username</Heading>
         <form onSubmit={onSubmit}>
-          <input
+          <Input
             name='username'
-            placeholder='myname'
+            placeholder='Username'
             value={formValue}
             onChange={onChange}
+            py={4}
           />
           <UsernameMessage
+            py={4}
             username={formValue}
             isValid={isValid}
             loading={loading}
           />
-          <Button type='submit' disabled={!isValid}>
+          <Button py={4} type='submit' disabled={!isValid}>
             Choose
           </Button>
-
-          <Heading as='h3'>Debug State</Heading>
-          <Box>
+          <Stack spacing={8}>
             Username: {formValue}
             <br />
             Loading: {loading.toString()}
             <br />
             Username Valid: {isValid.toString()}
-          </Box>
+          </Stack>
         </form>
-      </section>
+      </Stack>
     )
   );
 }
 
 function UsernameMessage({ username, isValid, loading }) {
   if (loading) {
-    return <Text>Checking...</Text>;
+    return <Text py={4}>Checking...</Text>;
   } else if (isValid) {
-    return <Text>{username} is available!</Text>;
+    return (
+      <Text py={4}>
+        <b>{username}</b> is available!
+      </Text>
+    );
   } else if (username && !isValid) {
-    return <Text>That username is taken!</Text>;
+    return <Text py={4}>That username is taken!</Text>;
   } else {
-    return <p>Soething else</p>;
+    return <Text py={4}>It will be displayed publicly.</Text>;
   }
 }
